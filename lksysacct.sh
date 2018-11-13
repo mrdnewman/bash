da53a1eafc1be898ba2edc272f744ed2c99cdd6e
mrdnewman <misterdnewman@icloud.com>
2018-11-13 16:32:36 -0600

    lksysacct.sh
    Locks down system accounts.

---
+++ lksysacct.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for user in `awk -F: '($3 < 500) {print $1 }' /etc/passwd`; do
+
+if [ $user != "root" ]
+
+then
+
+/usr/sbin/usermod -L $user
+
+if [ $user != "sync" ] && [ $user != "shutdown" ] && [ $user != "halt" ]
+
+then /usr/sbin/usermod -s /sbin/nologin $user
+
+fi
+
+fi
+
+done
