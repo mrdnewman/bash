dc03878e64280997561f251fe0b3967426e33264
mrdnewman <misterdnewman@icloud.com>
2018-11-19 16:11:03 -0600

    Import From USB

--- lksysacct.sh
+++ lksysacct.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 
+    # -- Lock down system accounts ...
+    # -- lksysacct.sh
+    # --
+    # --
+
+
+SCRIPT_NAME=`basename $0`;
+
+
 for user in `awk -F: '($3 < 500) {print $1 }' /etc/passwd`; do
 
    if [ $user != "root" ]; then
