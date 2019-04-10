ef84b99b2e16e573f2154807f1fcacc5573c1a61
mrdnewman <misterdnewman@icloud.com>
2019-04-10 09:40:37 -0700

    Update and rename lksysacct.sh to lck_SysAccts.sh

--- lck_SysAccts.sh
+++ lck_SysAccts.sh
@@ -3,22 +3,17 @@
 
     # -- Lock down system accounts ...
     # -- lksysacct.sh
-    # --
-    # --
-
 
 SCRIPT_NAME=`basename $0`;
 
-
 for user in `awk -F: '($3 < 500) {print $1 }' /etc/passwd`; do
-
+   
    if [ $user != "root" ]; then
       /usr/sbin/usermod -L $user
+   fi
 
    if [ $user != "sync" ] && [ $user != "shutdown" ] && [ $user != "halt" ]; then
       /usr/sbin/usermod -s /sbin/nologin $user
-
-   fi
    fi
 
 done
